eb4afd92e21eb34a61a7c4c60a6eb152a35da2bf
BrandonLunaCastro <blunacastro2003@gmail.com>
2023-08-14 16:28:40 -0300

    BREAKING CHANGE: adjust ultimate details of styles and form validation

--- assets/js/form-validation.js
+++ assets/js/form-validation.js
@@ -1,16 +1,13 @@
-
 let form = document.getElementById("form")
-
     const removeClass = (inputActual) => {
         setTimeout(() => {
             inputActual.classList.remove("input__invalid");
             inputActual.classList.remove("input__valid");
-        }, 2000);
+        }, 2500);
     }
     const addClassInvalid = (inputActual) => {
         inputActual.classList.remove("input__valid")
         inputActual.classList.add("input__invalid")
-       // console.log(inputActual)
         showError(inputActual)   
     }
     const addClassValid = (inputActual) => {
@@ -18,32 +15,33 @@ let form = document.getElementById("form")
         inputActual.classList.remove("input__invalid")
     }
 
+    /* Funcion que aplica la etiqueta de error correspondiente al input */
     const showError = (input) => {
         switch (input) {
             case phone:
-                document.querySelector(".span__tel").dataset.content = "*This fild only accept numbers";
+                document.querySelector(".span__tel").dataset.content = "*This fild only accept minimum 8 numbers";
                 break;
             case email:
                 document.querySelector(".span__email").dataset.content = "*Enter a valid email address";
                 break;
             case lastName:
-                document.querySelector(".span__lastName").dataset.content = "*Only accepts uppercase and lowercase letters"
+                document.querySelector(".span__lastName").dataset.content = "*Only accepts uppercase and lowercase characters"
                 break;
             case names:
-                document.querySelector(".span__name").dataset.content = "*Only accepts uppercase and lowercase letters"
+                document.querySelector(".span__name").dataset.content = "*Only accepts uppercase and lowercase characters"
+                break;
+            case password :
+                document.querySelector(".span__password").dataset.content = "*Should be contain at least one number , one lowercase and uppercase character"
                 break;
             case rePassword:
                 document.querySelector(".span__rePassword").dataset.content = "*Password must be the same"
                 break;
             default:
-                console.log("no entra")
+                input.dataset = "*This field is required"
                 break;
         } 
 
     }
-
-    
-
     /* funcion manejadora del evento submit */
     const validation = (event) =>{       
         document.querySelectorAll(".form__input").forEach(element => {
@@ -56,39 +54,28 @@ let form = document.getElementById("form")
               }       
         });
     };
-    /* Funcion manejadora del evento input que verifica los datos ingresados */
+    /* Funcion manejadora del evento input que verifica la integridad de los datos */
     const detectInputValue = (e) => {
-        //console.log(e.target.id)
-        /* if(e.target.value !== ''){
-           e.target.classList.remove("input__invalid");
-           e.target.classList.add("input__valid");
-        } */
-        let input = e.target,
-            password = 'a'
-   
-        if(input.id === "phone"){        
-            (/^[0-9]+$/ig).test(input.value) ? addClassValid(input) : addClassInvalid(input);
-            
-        }else if(input.id === "email"){
-            (/\w+\@\w{3,}\.\w{2,3}/ig).test(input.value) ?  addClassValid(input) : addClassInvalid(input)
+        let input = e.target;
 
+        if(input.id === "phone"){        
+            (/^[0-9]{8,}$/ig).test(input.value) ? addClassValid(input) : addClassInvalid(input);    
+        }else if(input.id === "email"){
+            (/\w+\@\w{3,}\.\w{2,3}/ig).test(input.value) ?  addClassValid(input) : addClassInvalid(input);
         }else if(input.id === "names" || input.id === "lastName"){
             (/^[a-z]+$/ig).test(input.value) ?  addClassValid(input) : addClassInvalid(input);
-        
         }
         else if(input.id === "password" ){
-            input.value.length > 6 && input.value.length < 20 ? addClassValid(input) : addClassInvalid(input)
-            
+            (/^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])[0-9a-zA-Z]{8,}$/).test(input.value) ? addClassValid(input) : addClassInvalid(input);      
         }else if(input.id === "rePassword"){
-            let passwordValue = document.querySelector("#password").value  
-            passwordValue === input.value ? addClassValid(input) : addClassInvalid(input)
+            let passwordValue = document.querySelector("#password").value;  
+            passwordValue === input.value ? addClassValid(input) : addClassInvalid(input);
         }
         else{
             input.classList.add("input__invalid");
-           // removeClass(e.target)
+            removeClass(e.target);
         }
     }
 
-document.querySelectorAll(".form__input").forEach(element => element.addEventListener("input",detectInputValue))
-
+document.querySelectorAll(".form__input").forEach(element => element.addEventListener("input",detectInputValue));
 form.addEventListener("submit", validation);
